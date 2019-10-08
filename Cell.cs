30fc690cc04c8a85e359f1779e1d5488f2551f8e
ghchoi0427 <44546623+ghchoi0427@users.noreply.github.com>
2019-10-09 00:33:14 +0900

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    class Cell
+    {
+        int num;        // 2~2048 (OR MORE)
+        int cellNum;    // 0~15
+        int power;      // 1~
+        TextBox CellTextBox;
+
+        public Cell()
+        {
+            CellTextBox = new TextBox();
+            CellTextBox.Multiline = true;
+            CellTextBox.TextAlign = HorizontalAlignment.Center;
+            CellTextBox.Font = new Font(CellTextBox.Font.FontFamily, 60);
+            CellTextBox.Width = 100;
+            CellTextBox.Height = 100;
+        }
+    
+        Color[] colors = new Color[] { Color.Gray, Color.Orange,Color.LightBlue,
+            Color.LightGreen,Color.LightPink, Color.LightYellow, Color.Magenta,
+            Color.LightSalmon, Color.LemonChiffon,Color.Lavender,Color.Red };
+
+        bool HitEnd = false;        // true when hit boundary
+        bool isBlank;               
+
+        public int GetNum { get => num; set => num = value; }
+        public Color[] Colors { get => colors; set => colors = value; }
+        public bool IsBlank { get => isBlank; set => isBlank = value; }
+        public int Power { get => power; set => power = value; }
+        public TextBox GetBox { get => CellTextBox; set => CellTextBox = value; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        Cell[] cells = new Cell[16];
+
+        public Form1()
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                cells[i] = new Cell();
+                cells[i].GetBox.BackColor = cells[i].Colors[cells[i].Power];
+                cells[i].GetBox.Text = cells[i].GetNum.ToString();
+                cells[i].GetBox = cells[i].GetBox;
+
+                cells[i].GetBox.Location = new Point(50 + 100 * (i % 4), 50 + 100 * (i / 4));     
+                Controls.Add(cells[i].GetBox);
+            }
+
+            InitializeComponent();
+            Paint += Form1_Paint;
+            Load += Form1_Load;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Width = 800;
+            Height = 600;
+        }
+
+        void moveUp()
+        {
+
+        }
+
+        void moveDown()
+        {
+
+        }
+
+        void moveRight()
+        {
+
+        }
+
+        void moveLeft()
+        {
+
+        }
+
+        void GenerateNum()
+        {
+
+        }
+
+        
+
+
+    }
+}
